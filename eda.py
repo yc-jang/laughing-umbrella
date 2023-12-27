1e733262253a54ab7e3c1360c653d52e2c4facf8
yc-jang <64676741+yc-jang@users.noreply.github.com>
2023-12-27 15:17:56 +0900

    Update eda.py

--- eda.py
+++ eda.py
@@ -7,18 +7,23 @@ from sklearn.decomposition import PCA
 import plotly.express as px
 
 class EDA:
-    def __init__(self, dataset):
-        self.dataset = dataset
-        self.df = None
+    def __init__(self, df, target_column, drop_columns=None, mini=False, sample_fraction=0.1):
+        self.df = df.copy()
+        self.target_column = target_column
+        self.mini = mini
 
-    def load_data(self):
-        if self.dataset == 'breast_cancer':
-            data = load_breast_cancer()
-            self.df = pd.DataFrame(data.data, columns=data.feature_names)
-            self.df['target'] = data.target
-        else:
-            raise ValueError("Unsupported dataset")
-    
+        # Drop specified columns if any
+        if drop_columns:
+            self.df.drop(drop_columns, axis=1, inplace=True)
+
+        if self.target_column not in self.df.columns:
+            raise ValueError(f"Target column '{self.target_column}' not found in dataset")
+
+        # 샘플링 옵션
+        if self.mini:
+            self.df = self.df.sample(frac=sample_fraction, random_state=42)
+
+
     def display_basic_info(self):
         print("Basic Information of the Dataset:")
         print("Shape:", self.df.shape)
@@ -31,31 +36,64 @@ class EDA:
         fig = px.imshow(corr_matrix, title="Correlation Matrix")
         fig.show()
 
-    def perform_rfe(self, n_features=5):
-        X = self.df.drop('target', axis=1)
-        y = self.df['target']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-        model = LogisticRegression(max_iter=5000)
-        rfe = RFE(model, n_features_to_select=n_features)
-        rfe.fit(X_train, y_train)
-        self.rfe_features = pd.Series(rfe.support_, index=X.columns)
+    def perform_rfe(self, n_features=5, force=False):
+        if not hasattr(self, 'rfe_features') or force:
+            X = self.df.drop(self.target_column, axis=1)
+            y = self.df[self.target_column]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+            model = LinearRegression()
+            rfe = RFE(model, n_features_to_select=n_features)
+            rfe.fit(X_train, y_train)
+            self.rfe_features = pd.Series(rfe.support_, index=X.columns)
         return self.rfe_features
 
-    def plot_rfe_results(self):
+    def plot_rfe_results(self, n_features=5):
+        self.perform_rfe(n_features)
         fig = px.bar(self.rfe_features, title="Feature Importance (RFE)")
         fig.show()
 
-    def perform_pca(self, n_components=2):
-        X = self.df.drop('target', axis=1)
-        pca = PCA(n_components=n_components)
-        components = pca.fit_transform(X)
-        self.pca_df = pd.DataFrame(components, columns=[f'PC{i+1}' for i in range(n_components)])
-        self.pca_df['target'] = self.df['target']
+    def perform_pca(self, n_components=2, force=False):
+        if not hasattr(self, 'pca_df') or force:
+            X = self.df.drop(self.target_column, axis=1)
+            pca = PCA(n_components=n_components)
+            components = pca.fit_transform(X)
 
-    def plot_pca_results(self):
+            # PCA 결과 DataFrame 생성 및 인덱스 맞추기
+            self.pca_df = pd.DataFrame(components, columns=[f'PC{i+1}' for i in range(n_components)])
+            self.pca_df.index = self.df.index  # 원본 DataFrame의 인덱스를 pca_df에 복사
+            self.pca_df['target'] = self.df[self.target_column]
+
+    def plot_pca_results(self, n_components=2):
+        self.perform_pca(n_components)
         fig = px.scatter(self.pca_df, x='PC1', y='PC2', color='target', title="PCA (2 components)")
         fig.show()
 
+    def perform_pls(self, n_components=2, force=False):
+        if not hasattr(self, 'pls_scores') or force:
+            X = self.df.drop(self.target_column, axis=1)
+            y = self.df[self.target_column]
+            self.pls = PLSRegression(n_components=n_components)
+            self.pls.fit(X, y)
+            self.pls_scores = self.pls.transform(X)
+
+    def plot_pls_results(self, n_components=2):
+        self.perform_pls(n_components)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=self.pls_scores[:, 0], y=self.pls_scores[:, 1], mode='markers',
+                                 marker=dict(color=self.df[self.target_column]), text=self.df.index))
+        fig.update_layout(title="PLS (2 components)", xaxis_title="PLS 1", yaxis_title="PLS 2")
+        fig.show()
+    
+    def perform_umap(self, n_components=2, n_neighbors=15, min_dist=0.1, metric='euclidean', force=False):
+        if not hasattr(self, 'umap_results') or force:
+            X = self.df.drop(self.target_column, axis=1)
+            self.umap_model = umap.UMAP(n_components=n_components, n_neighbors=n_neighbors, min_dist=min_dist, metric=metric)
+            self.umap_results = self.umap_model.fit_transform(X)
+
+    def plot_umap_results(self, n_components=2, n_neighbors=15, min_dist=0.1, metric='euclidean'):
+        self.perform_umap(n_components, n_neighbors, min_dist, metric)
+        fig = px.scatter(x=self.umap_results[:, 0], y=self.umap_results[:, 1], color=self.df[self.target_column], title="UMAP Visualization")
+        fig.show()
 # 사용 예시
 eda = EDA('breast_cancer')
 eda.load_data()
